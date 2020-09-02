eb53876d7d4e73585773991d02df011204ca240c
Priya Debnath <priya.debnath@ad.infosys.com>
2020-09-02 17:13:02 +0530

    Formik Tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withFormik,Form, Field } from 'formik';
+import Yup from 'yup'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=({
+  values,
+  // handleChange
+  // handleSubmit
+  errors,
+  touched,
+  isSubmitting
+})=>(
+  // <form onSubmit={handleSubmit}>
+  <Form>
+    {/* <input type="email" name="email" placeholder="Email" value={values.email} onChange={handleChange}/>
+    <input type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange}/> */}
+    <div>
+  {touched.email && errors.email && <p>{errors.email}</p>}
+    <Field type="email" name="email" placeholder="Email"/>
     </div>
-  );
-}
-
-export default App;
+    <div>
+    {touched.password && errors.password && <p>{errors.password}</p>}
+    <Field type="password" name="password" placeholder="Password"/>
+    </div>
+    <label>
+    <Field type="checkbox" name="newsletter" checked={values.newsletter}/>
+    Join our newsletter
+    </label>
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
+    <button disabled={isSubmitting}>Submit</button>
+  </Form>
+)
+const FormikApp= withFormik({
+  mapPropsToValues({email,password,newsletter,plan}){
+    return{
+      email: email || '',
+      password: password || '',
+      newletter: newsletter || false,
+      plan: plan || 'free'
+    }
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email('Email not valid').required('Email is required'),
+    password: Yup.string().min(9,'Password must be 9 characters or longer').required('Password is required')
+  }),
+  handleSubmit(values, { resetForm,setErrors,setSubmitting}){
+    setTimeout(()=>{
+      if(values.email === 'andrew@test.io'){
+        setErrors({ email: 'That email is already taken'})
+      } else{
+        resetForm()
+      }
+      setSubmitting(false)
+    },2000)
+    // console.log(values)
+  }
+})(App)
+export default FormikApp;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App email="priya@gmail.com"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
